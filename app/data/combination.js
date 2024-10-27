01fc129ea488df936e0b65d977d3c1bf6afb7f64
The Sakib Dev <139985754+thesakibdev@users.noreply.github.com>
2024-10-27 23:20:36 +0600

    bug fixed
    bug fixed and code optimized

---
+++ app/data/combination.js
@@ -0,0 +1,519 @@
+import Logo1 from "../logos/combination-mark/Logo1";
+import Logo2 from "../logos/combination-mark/Logo2";
+import Logo3 from "../logos/combination-mark/Logo3";
+import Logo4 from "../logos/combination-mark/Logo4";
+import Logo5 from "../logos/combination-mark/Logo5";
+import Logo6 from "../logos/combination-mark/Logo6";
+import Logo7 from "../logos/combination-mark/Logo7";
+import Logo8 from "../logos/combination-mark/Logo8";
+import Logo9 from "../logos/combination-mark/Logo9";
+import Logo10 from "../logos/combination-mark/Logo10";
+import Logo11 from "../logos/combination-mark/Logo11";
+import Logo12 from "../logos/combination-mark/Logo12";
+import Logo13 from "../logos/combination-mark/Logo13";
+import Logo14 from "../logos/combination-mark/Logo14";
+import Logo15 from "../logos/combination-mark/Logo15";
+import Logo16 from "../logos/combination-mark/Logo16"; 
+import Logo17 from "../logos/combination-mark/Logo17";
+import Logo18 from "../logos/combination-mark/Logo18";
+import Logo19 from "../logos/combination-mark/Logo19";
+import Logo20 from "../logos/combination-mark/Logo20";
+import Logo21 from "../logos/combination-mark/Logo21";
+import Logo22 from "../logos/combination-mark/Logo22";
+import Logo23 from "../logos/combination-mark/Logo23";
+import Logo24 from "../logos/combination-mark/Logo24";
+import Logo25 from "../logos/combination-mark/Logo25";
+import Logo26 from "../logos/combination-mark/Logo26";
+import Logo27 from "../logos/combination-mark/Logo27";
+import Logo28 from "../logos/combination-mark/Logo28";
+import Logo29 from "../logos/combination-mark/Logo29";
+import Logo30 from "../logos/combination-mark/Logo30";
+import Logo31 from "../logos/combination-mark/Logo31";
+import Logo32 from "../logos/combination-mark/Logo32";
+import Logo33 from "../logos/combination-mark/Logo33";
+import Logo34 from "../logos/combination-mark/Logo34";
+import Logo35 from "../logos/combination-mark/Logo35";
+import Logo36 from "../logos/combination-mark/Logo36";
+import Logo37 from "../logos/combination-mark/Logo37";
+import Logo38 from "../logos/combination-mark/Logo38";
+import Logo39 from "../logos/combination-mark/Logo39";
+import Logo40 from "../logos/combination-mark/Logo40";
+import Logo41 from "../logos/combination-mark/Logo41";
+import Logo42 from "../logos/combination-mark/Logo42";
+import Logo43 from "../logos/combination-mark/Logo43";
+import Logo44 from "../logos/combination-mark/Logo44";
+import Logo45 from "../logos/combination-mark/Logo45";
+import Logo46 from "../logos/combination-mark/Logo46";
+import Logo47 from "../logos/combination-mark/Logo47";
+import Logo48 from "../logos/combination-mark/Logo48";
+import Logo49 from "../logos/combination-mark/Logo49";
+import Logo50 from "../logos/combination-mark/Logo50";
+import Logo51 from "../logos/combination-mark/Logo51";
+import Logo52 from "../logos/combination-mark/Logo52";
+import Logo53 from "../logos/combination-mark/Logo53";
+import Logo54 from "../logos/combination-mark/Logo54";
+import Logo55 from "../logos/combination-mark/Logo55";
+import Logo56 from "../logos/combination-mark/Logo56";
+import Logo57 from "../logos/combination-mark/Logo57";
+
+export const combinationMark = [
+    { id: 1, 
+      logo: <Logo1 />,
+      category: "combination",
+      name: "logo1",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 2, 
+      logo: <Logo2 />,
+      category: "combination",
+      name: "logo2",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    
+    { id: 3, 
+      logo: <Logo3 />,
+      category: "combination",
+      name: "logo3",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 4, 
+      logo: <Logo4 />,
+      category: "combination",
+      name: "logo4",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 5, 
+      logo: <Logo5 />,
+      category: "combination",
+      name: "logo5",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 6, 
+      logo: <Logo6 />,
+      category: "combination",
+      name: "logo6",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 7, 
+      logo: <Logo7 />,
+      category: "combination",
+      name: "logo7",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 8, 
+      logo: <Logo8 />,
+      category: "combination",
+      name: "logo8",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 9, 
+      logo: <Logo9 />,
+      category: "combination",
+      name: "logo9",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 10, 
+      logo: <Logo10 />,
+      category: "combination",
+      name: "logo10",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 11, 
+      logo: <Logo11 />,
+      category: "combination",
+      name: "logo11",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 12, 
+      logo: <Logo12 />,
+      category: "combination",
+      name: "logo12",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 13, 
+      logo: <Logo13 />,
+      category: "combination",
+      name: "logo13",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 14, 
+      logo: <Logo14 />,
+      category: "combination",
+      name: "logo14",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 15, 
+      logo: <Logo15 />,
+      category: "combination",
+      name: "logo15",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 16, 
+      logo: <Logo16 />,
+      category: "combination",
+      name: "logo16",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 17, 
+      logo: <Logo17 />,
+      category: "combination",
+      name: "logo17",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 18, 
+      logo: <Logo18 />,
+      category: "combination",
+      name: "logo18",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 19, 
+      logo: <Logo19 />,
+      category: "combination",
+      name: "logo19",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 20, 
+      logo: <Logo20 />,
+      category: "combination",
+      name: "logo20",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 21, 
+      logo: <Logo21 />,
+      category: "combination",
+      name: "logo21",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 22, 
+      logo: <Logo22 />,
+      category: "combination",
+      name: "logo22",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 23, 
+      logo: <Logo23 />,
+      category: "combination",
+      name: "logo23",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 24, 
+      logo: <Logo24 />,
+      category: "combination",
+      name: "logo24",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 25, 
+      logo: <Logo25 />,
+      category: "combination",
+      name: "logo25",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 26, 
+      logo: <Logo26 />,
+      category: "combination",
+      name: "logo26",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 27, 
+      logo: <Logo27 />,
+      category: "combination",
+      name: "logo27",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 28, 
+      logo: <Logo28 />,
+      category: "combination",
+      name: "logo28",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 29, 
+      logo: <Logo29 />,
+      category: "combination",
+      name: "logo29",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 30, 
+      logo: <Logo30 />,
+      category: "combination",
+      name: "logo30",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 31, 
+      logo: <Logo31 />,
+      category: "combination",
+      name: "logo31",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 32, 
+      logo: <Logo32 />,
+      category: "combination",
+      name: "logo32",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 33, 
+      logo: <Logo33 />,
+      category: "combination",
+      name: "logo33",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 34, 
+      logo: <Logo34 />,
+      category: "combination",
+      name: "logo34",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 35, 
+      logo: <Logo35 />,
+      category: "combination",
+      name: "logo35",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 36, 
+      logo: <Logo36 />,
+      category: "combination",
+      name: "logo36",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 37, 
+      logo: <Logo37 />,
+      category: "combination",
+      name: "logo37",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 38, 
+      logo: <Logo38 />,
+      category: "combination",
+      name: "logo38",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 39, 
+      logo: <Logo39 />,
+      category: "combination",
+      name: "logo39",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 40, 
+      logo: <Logo40 />,
+      category: "combination",
+      name: "logo40",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 41, 
+      logo: <Logo41 />,
+      category: "combination",
+      name: "logo41",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 42, 
+      logo: <Logo42 />,
+      category: "combination",
+      name: "logo42",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 43, 
+      logo: <Logo43 />,
+      category: "combination",
+      name: "logo43",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 44, 
+      logo: <Logo44 />,
+      category: "combination",
+      name: "logo44",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 45, 
+      logo: <Logo45 />,
+      category: "combination",
+      name: "logo45",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 46, 
+      logo: <Logo46 />,
+      category: "combination",
+      name: "logo46",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 47, 
+      logo: <Logo47 />,
+      category: "combination",
+      name: "logo47",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 48, 
+      logo: <Logo48 />,
+      category: "combination",
+      name: "logo48",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 49, 
+      logo: <Logo49 />,
+      category: "combination",
+      name: "logo49",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 50, 
+      logo: <Logo50 />,
+      category: "combination",
+      name: "logo50",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 51, 
+      logo: <Logo51 />,
+      category: "combination",
+      name: "logo51",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 52, 
+      logo: <Logo52 />,
+      category: "combination",
+      name: "logo52",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 53, 
+      logo: <Logo53 />,
+      category: "combination",
+      name: "logo53",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 54, 
+      logo: <Logo54 />,
+      category: "combination",
+      name: "logo54",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 55, 
+      logo: <Logo55 />,
+      category: "combination",
+      name: "logo55",
+      color: "black",
+      size: "small",
+      random: true
+    },
+    { id: 56, 
+      logo: <Logo56 />,
+      category: "combination",
+      name: "logo56",
+      color: "white",
+      size: "small",
+      random: true
+    },
+    { id: 57, 
+      logo: <Logo57 />,
+      category: "combination",
+      name: "logo57",
+      color: "black",
+      size: "small",
+      random: true
+    },
+];
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import DynamoLogos from "./component/DynamoLogos";
 export default function Home() {
   return (
     <div className="">
-      <DynamoLogos category="abstract"/>
-      <DynamoLogos category="combination-mark" />
+      <DynamoLogos random />
     </div>
   );
 }
