35d616f751211c2de841c76c8a34e81d5760cf0a
The Sakib Dev <139985754+thesakibdev@users.noreply.github.com>
2024-10-31 02:11:25 +0600

    Brand Logo
    brand logo component complete

---
+++ app/data/brand.js
@@ -0,0 +1,43 @@
+import Adidas from "@/logos/brand/Adidas";
+import Amazon from "@/logos/brand/Amazon";
+import Facebook from "@/logos/brand/Facebook";
+import Google from "@/logos/brand/Google";
+
+export const brand = [
+  {
+    id: 1,
+    logo: <Adidas />,
+    category: "brand",
+    name: "Adidas",
+    color: "black",
+    size: "small",
+    random: true,
+  },
+  {
+    id: 2,
+    logo: <Amazon />,
+    category: "brand",
+    name: "Amazon",
+    color: "black",
+    size: "small",
+    random: true,
+  },
+  {
+    id: 3,
+    logo: <Facebook />,
+    category: "brand",
+    name: "Facebook",
+    color: "black",
+    size: "small",
+    random: true,
+  },
+  {
+    id: 4,
+    logo: <Google />,
+    category: "brand",
+    name: "Google",
+    color: "black",
+    size: "small",
+    random: true,
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import DynamoLogos from "./component/DynamoLogos";
 export default function Home() {
   return (
     <div className="">
-      <DynamoLogos name="logo9" />
+      <DynamoLogos name="logo57" />
     </div>
   );
 }
