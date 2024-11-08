0f74c98369b57f2f4d2db3ac9f26a738e3add7f5
The Sakib Dev <139985754+thesakibdev@users.noreply.github.com>
2024-11-08 11:40:13 +0600

    try to fixed build bug

--- app/logos/brand/LiveStorm.tsx
+++ app/logos/brand/LiveStorm.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 export default function LiveStorm({
   width = "100",
   height = "17",
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,10 @@ export default defineConfig({
   format: ['cjs', 'esm'],
   dts: true,
   clean: true,
+  treeshake: false,  // Disable tree-shaking to prevent removal of imports
   entry: ['index.ts'],
   skipNodeModulesBundle: true,
   shims: true,
+  jsxFactory: 'React.createElement',
+  jsxFragment: 'React.Fragment',
 });
